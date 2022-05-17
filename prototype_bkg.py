2db64af88e2f3421e5b7c3bc611c6d097cb4d86c
Osas <Osaghae2305@gmail.com>
2022-05-17 12:37:21 +0100

    Added some comments

--- prototype_bkg.py
+++ prototype_bkg.py
@@ -3,22 +3,26 @@ pygame.init() # initialises all imported pygame modules
 
 clock = pygame.time.Clock()
 
+# Stored RGB values inside variables to save time instead of re writing each rgb value of a sprite
 WHITE = (255,255,255)
 RED = (255,0,0)
 BLACK = (0,0,0)
 
 class Block(pygame.sprite.Sprite):
-    def __init__(self,pos,width,height):
+    def __init__(self,pos,width,height): # pos = position, width,height = width and height of the blocks
         super().__init__()
         self.block_height = width
         self.block_width = height
 
+        # creates an image
         self.image = pygame.Surface([self.block_width,self.block_height])
 
+        # Puts this image in a specific location in the screen
         self.rect = self.image.get_rect(topleft=pos)
 
 
     def update(self,surface):
+        # draws the sprite in the display
         pygame.draw.rect(surface,(WHITE),self.rect)
     
 
@@ -26,13 +30,14 @@ class Player(pygame.sprite.Sprite):
     def __init__(self,pos):# pos = position
         super().__init__()# Calls parent class constructor
         
-        # creates an image and fills the image in with a colour
+        # creates an image
         self.image = pygame.Surface([25,25]) 
 
         # Puts this image in a specific location in the screen
         self.rect = self.image.get_rect(topleft = pos)
 
     def update(self,surface):
+        # draws the sprite in the display
         pygame.draw.rect(surface,RED,self.rect)
 
 
@@ -41,7 +46,7 @@ class Game:
     def __init__(self):
         # level layout 
         # X represents the block and P represents the player
-        # 
+        # 15 spaces in one row
         self.level =[
             "               ",
             "              X",
@@ -63,8 +68,8 @@ class Game:
         return len(self.level)
         
 
-    def setUpLevel(self):
-        self.blocks = pygame.sprite.Group()
+    def setUpLevel(self): # This function creates all the sprites that are going to be used in this game
+        self.blocks = pygame.sprite.Group()# creates a group that will contain all the block sprites
         self.player = pygame.sprite.GroupSingle()# creates a sprite group class that can only contain one sprite which in this case would be player
         # enumerate() returns (index of the value,the actual value)
         for row_index,row in enumerate(self.level):
@@ -72,25 +77,25 @@ class Game:
                 # if an X is found then the programme creates a Block class and passes it a certain position in the screen
                 if col == "X":
                     block_sprite = Block((col_index*self.block_width,row_index*self.block_height),self.block_width,self.block_height)
-                    self.blocks.add(block_sprite)
-                # if a P is found then a player class is created with and it passes a certain position in the screen
+                    self.blocks.add(block_sprite) # Adds the block to the blocks group
+                # if a P is found then a player class is created and it passes a certain position in the screen
                 # then it adds the player into the player group
                 elif col == "P":
                     player_sprite = Player((col_index*self.block_width,(row_index*self.block_height)+25))
                     self.player.add(player_sprite)
 
-    def run(self,surface):
+    def run(self,surface): # draws all the sprites to the screen
         self.blocks.update(surface)
         self.player.update(surface)
 
-
+############################# Main Game ###################################################
 
 game = Game() # initialises game class
-game.setUpLevel()
+game.setUpLevel() # Creates all the sprites
 
-# creates a pygame window and names it
-DISPLAYSURF = pygame.display.set_mode((700,game.getBlockHeight()*game.getNumofRows()))
-pygame.display.set_caption("TEst window")
+# creates a pygame window and names it 
+DISPLAYSURF = pygame.display.set_mode((700,game.getBlockHeight()*game.getNumofRows())) #window height is adjusted by how many rows the level has and how tall the blocks are
+pygame.display.set_caption("Test window")
 
 run = True
 
@@ -113,5 +118,5 @@ while run:# main game loop
     # Caps the framerate to 60 Frames per second
     clock.tick(60)
     
-
+# Closes all pygame modules
 pygame.quit()
