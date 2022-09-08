34e7eca672194a57a41f0809aed5258be8f7892c
Osas <Osaghae2305@gmail.com>
2022-09-08 16:29:42 +0100

    Made some additions in main.py

--- main.py
+++ main.py
@@ -10,6 +10,11 @@ pygame.display.set_caption("Test window")
 run = True
 
 while run:# main game loop
+
+    for event in pygame.event.get():# Event handler
+        # ends game loop if the top right cross button or the escape key is pressed
+        if event.type == pygame.QUIT or event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
+            run = False
     
     # Caps the framerate to 60 Frames per second
     clock.tick(60)
